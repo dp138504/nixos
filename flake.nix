0e53c30b64084f43eee2e34f0c2d05230c88cda0
Dylan A. Pitts <pitts.dylan@gmail.com>
2026-04-28 22:36:30 -0500

    remove old host

--- flake.nix
+++ flake.nix
@@ -152,17 +152,6 @@
             ./modules/home-manager
           ];
         };
-        "dap@dylan-acenet" = home-manager.lib.homeManagerConfiguration {
-          # Work Config
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = {
-            inherit inputs outputs nix-colors;
-          };
-          modules = [
-            ./hosts/dylan-acenet/home.nix
-            ./modules/home-manager
-          ];
-        };
       };
     };
 }
